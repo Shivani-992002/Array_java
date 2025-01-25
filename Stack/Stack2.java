dc2356588a6663ecc174cd4e8f91e0976c2f517d
Shivani-992002 <itsshivanirwt134@gmail.com>
2025-01-25 22:08:41 +0530

    Add files via upload

---
+++ Stack/Stack2.java
@@ -0,0 +1,68 @@
+// PUSH POP PEEK ISEMPTY FUNCTION IN LINKED LIST
+
+import java.util.*;
+public class Stack2{
+
+    static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    static class Stack{
+        static Node head=null;
+
+        //isEmpty
+        public static boolean isEmpty(){
+            return head==null;
+        }
+
+        //push
+        public static void push(int data){
+            Node newNode=new Node(data);
+            if(isEmpty()){
+                head=newNode;
+                return;
+            }
+            newNode.next=head;
+            head=newNode;
+        }
+
+        //pop
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top=head.data;
+            head=head.next;
+            return top;
+        }
+
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top=head.data;
+            return top;
+        }                                        
+        
+    }
+
+    public static void main(String args[]){
+        Stack s =new Stack();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
