3a3b90bcb395241dc4975440b9ca248d237bc990
Shivani-992002 <itsshivanirwt134@gmail.com>
2025-01-29 22:34:55 +0530

    Add files via upload

---
+++ Queue/Queue4.java
@@ -0,0 +1,16 @@
+//java collection framework
+import java.util.*;
+public class Queue4{
+    public static void main(String args[]){
+        Queue<Integer> q=new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
